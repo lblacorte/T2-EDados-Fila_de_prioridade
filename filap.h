2aaaefce3516d9003f5e2dacc003db666b7206c1
Leandro Lacorte <129808495+lblacorte@users.noreply.github.com>
2023-11-13 21:48:04 -0300

    Update filap.h

--- filap.h
+++ filap.h
@@ -1,17 +1,20 @@
 #ifndef FILAP_H
 #define FILAP_H
 
+// Estrutura para representar um item na fila de prioridade
 typedef struct {
-  int dado;
-  int prioridade;
+  int dado;         // Dado associado ao item
+  int prioridade;   // Prioridade do item na fila
 } Item;
 
+// Estrutura para representar a fila de prioridade
 typedef struct {
-  Item* itens; //Vetor de itens
-  int n; //Número atual de itens
-  int tamanho; //Tamanho máximo da fila
+  Item* itens;      // Vetor de itens
+  int n;            // Número atual de itens na fila
+  int tamanho;      // Tamanho máximo da fila
 } FilaP;
 
+// Função para criar uma nova fila de prioridade com o tamanho especificado
 FilaP* criarFilaP(int tam);
 
 #endif // FILAP_H
